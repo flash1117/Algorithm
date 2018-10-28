fba7c15f8c14e28b72dbe7a84303f958206d7c7c
kitae <flash1117@naver.com>
2018-10-28 18:29:09 +0900

    modify midterm 1

--- MidTerm01/MidTerm01/MidTerm01.cpp
+++ MidTerm01/MidTerm01/MidTerm01.cpp
@@ -2,9 +2,36 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define SIZE 20
+
+int graph[SIZE][SIZE];
+
 int main() {
 
+	int T, N, s, t, L;
+	int T_case = 0;
+	char buffer[1000];
+	FILE *fp = fopen("input1.txt", "r");
 
+	fscanf(fp, "%d", &T);
+
+	while (!feof(fp)) {
+
+		fscanf(fp, "%d", &N);
+
+		for (int i = 0; i < SIZE; i++) {
+
+			for (int j = 0; j < SIZE; j++) {
+
+
+			}
+		}
+
+	}
+
+
+
+	fclose(fp);
 
 	return 0;
 }
