8bd51e89e30346b255095fa3ad6e2dbed288f626
kitae <flash1117@naver.com>
2018-10-29 17:18:08 +0900

    need findpath

--- MidTerm01/MidTerm01/MidTerm01.cpp
+++ MidTerm01/MidTerm01/MidTerm01.cpp
@@ -11,7 +11,7 @@ void print_graph(int N) {
 	for (int i = 0; i < N; i++) {
 
 		for (int j = 0; j < N; j++) {
-			printf("%d " ,graph[i][j]);
+			printf("%d ", graph[i][j]);
 		}
 		printf("\n");
 
@@ -23,36 +23,24 @@ void init_graph() {
 
 	for (int i = 0; i < SIZE; i++) {
 
-		for (int j = 0; j < SIZE; j++) 
+		for (int j = 0; j < SIZE; j++)
 			graph[i][j] = 0;
-			
+
 	}
 
 }
+	
+void complete() {
 
-void complete(char buffer[], int row) {
 
-	int temp = atoi(buffer);
-	int count = 0;
-	int r;
 
-	for (int i = 0; i < SIZE; i++) {
-
-		r = temp % 10;
-		temp = temp / 10;
-
-		if (count == r)
-			break;
-
-		graph[row][r] = 1;
-		count++;
-	}
 }
+
 int main() {
 
 	int T, N, s, t, L;
-	int T_case = 0;
-	char buffer[1000];
+	int count;
+	int temp;
 	FILE *fp = fopen("input1.txt", "r");
 
 	if (fp == NULL) {
@@ -64,14 +52,17 @@ int main() {
 
 	while (!feof(fp)) {
 		init_graph();
-		fscanf(fp, "%d", &N);
-		printf("N = %d!\n", N);
+		fscanf(fp, "%d ", &N);
+		printf("N = %d\n", N);
 
-		for (int i = 0; i <= N; i++) {
+		for (int i = 0; i < N; i++) {
 
-			fgets(buffer, 1000, fp);
-			printf("%s\n", buffer);
-			complete(buffer, i);
+			fscanf(fp, "%d ", &count);
+			for (int j = 0; j < count; j++) {
+				fscanf(fp, "%d ", &temp);
+				graph[i][temp] = 1;
+			}
+
 		}
 		fscanf(fp, "%d %d %d", &s, &t, &L);
 		printf("s = %d, t = %d, L = %d\n", s, t, L);
