f9306ee6c352f6eb08287fb4fd022ab01d79d385
김기태 <mollangzzang@gmail.com>
2019-06-08 22:18:10 +0900

    2019-06-08 maze

--- 2178_maze/2178.cpp
+++ 2178_maze/2178.cpp
@@ -1,92 +1,61 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
 #include <queue>
+#include <string>
 
 using namespace std;
 
-#define MAX 100
-
 int N, M;
-int maze[MAX][MAX];
-bool visited[MAX][MAX];
+bool visited[101][101];
+bool map[101][101];
 
-typedef struct {
-	int y, x, pathLength; // 좌표와 현재까지의 길이
+int dx[] = { 0,0,-1,1 };
+int dy[] = { 1,-1,0,0 };
 
-}dir;
+struct pt {
+	int x, y, len;
+};
 
-int minStep(int y, int x, int pathLength) {
-
-	queue<dir> q;
-	int result = 0;
-	dir start = { y,x,pathLength };
-	q.push(start);
-
-
-	while (!q.empty()) {
-
-		int curY = q.front().y;
-		int curX = q.front().x;
-		int curLength = q.front().pathLength;
-
-		if (curY == N - 1 && curX == M - 1) {
-			result = curLength;
-			break;
-		}
-		q.pop();
-		visited[y][x] = true;
-
-		//east
-		if (curX + 1 < M && maze[curY][curX + 1] && !visited[curY][curX + 1]) {
-
-			dir east = { curY, curX + 1, curLength + 1 };
-			visited[curY][curX + 1] = true;
-			q.push(east);
-
-
-		}
-
-		//west
-		if (curX - 1 >= 0 && maze[curY][curX - 1] && !visited[curY][curX - 1]) {
-
-			dir west = { curY, curX - 1, curLength + 1 };
-			visited[curY][curX - 1] = true;
-			q.push(west);
-
-		}
-
-		//south
-		if (curY + 1 < M && maze[curY + 1][curX] && !visited[curY + 1][curX]) {
-
-			dir south = { curY + 1 , curX, curLength + 1 };
-			visited[curY + 1][curX] = true;
-			q.push(south);
-		}
-
-		//North
-		if (curY - 1 >= 0 && maze[curY - 1][curX] && !visited[curY - 1][curX]) {
-
-			dir north = { curY - 1, curX, curLength + 1 };
-			visited[curY - 1][curX] = true;
-			q.push(north);
-		}
-
-	}
-	return result;
+bool inBoundary(int x, int y) {
+	if (x<0 || y<0 || x>N - 1 || y>M - 1) return false;
+	return true;
 }
 
-int main(void) {
+int BFS() {
+	queue<pt> q;
+	q.push({ 0,0,1 });
+	visited[0][0] = true;
 
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
+	while (!q.empty()) {
+		int curX = q.front().x;
+		int curY = q.front().y;
+		int len = q.front().len;
+		q.pop();
 
-		string temp;
-		cin >> temp;
-		for (int j = 0; j < M; j++)
-			maze[i][j] = temp[j] - '0';
+		if (curX == N - 1 && curY == M - 1) return len;
 
+		for (int i = 0; i < 4; i++) {
+			int nextX = curX + dx[i];
+			int nextY = curY + dy[i];
+			if (inBoundary(nextX, nextY) && map[nextX][nextY] && !visited[nextX][nextY]) {
+				q.push({ nextX,nextY,len + 1 });
+				visited[nextX][nextY] = true;
+			}
+		}
 	}
-	memset(visited, false, sizeof(visited));
-	cout << minStep(0, 0, 1) << endl;
+}
+
+
+int main() {
+	cin >> N >> M;
+	string s;
+	for (int i = 0; i < N; i++) {
+		cin >> s;
+		for (int j = 0; j < M; j++) {
+			map[i][j] = s[j] - '0';
+		}
+	}
+	cout << BFS();
+
 	return 0;
 }
