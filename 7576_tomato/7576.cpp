b1197aa44768ced1f0509c2ccc38f7745d1323bf
김기태 <mollangzzang@gmail.com>
2019-06-15 07:43:46 +0900

    2019-06-15 update

--- 7576_tomato/7576.cpp
+++ 7576_tomato/7576.cpp
@@ -8,12 +8,10 @@ int M, N; // 2이상 1000이하
 int map[1001][1001];
 bool visited[1001][1001];
 
-
 int dx[] = { 0,0,-1,1 };
 int dy[] = { -1,1,0,0 };
 
 typedef struct {
-
 	int x, y;
 }pt;
 
@@ -22,18 +20,61 @@ bool inBoundary(int x, int y) {
 	return true;
 }
 
+queue<pt> q;
+
 int BFS() {
-	queue<pt> q;
-	q.push({})
 
+	int cnt = 0;
 
+	while (!q.empty()) {
+
+		int curX = q.front().x;
+		int curY = q.front().y;
+		q.pop();
+
+		cout << curX << curY << endl;
+
+		for (int j = 0; j < 4; j++) {
+			int nextX = curX + dx[j];
+			int nextY = curY + dy[j];
+
+			cout << "Boundary Value : " << inBoundary(nextX, nextY) << endl;
+			cout << "next map (x,y) : " << map[nextX][nextY] << endl;
+			cout << "M : " << M << " N : " << N << endl;
+			cout << "nextX : " << nextX << " nextY : " << nextY << endl;
+			cout << "!visit : " << !visited[nextX][nextY] << endl;
+
+			if (inBoundary(nextX, nextY) && !map[nextX][nextY] && !visited[nextX][nextY]) {
+				q.push({ nextX, nextY });
+				cout << "execute queue push" << endl;
+				visited[nextX][nextY] = true;
+			}
+			else if (map[nextX][nextY] == -1)
+				continue;
+
+		}
+		cnt++;
+	}
+
+	return cnt;
 }
-
-
 
 int main() {
 
 	cin >> M >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+
+			cin >> map[i][j];
+
+			if (map[i][j] == 1)
+				q.push({ i,j });
+				
+		}
+	}
+
 	cout << BFS();
+
 	return 0;
 }
