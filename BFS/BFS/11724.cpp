1cc16aa062f7b1f679011499607cf09bfedcc2f8
김기태 <mollangzzang@gmail.com>
2019-06-20 17:04:28 +0900

    2019-06-20 update-3

---
+++ BFS/BFS/11724.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+
+int N, M;
+
+int main() {
+
+	int u, v;
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++) {
+
+		cin >> u >> v;
+
+	}
+
+
+	return 0;
+}
