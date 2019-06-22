25817dfa71916fbabaf8e2ad7f3e661c35296ffa
김기태 <mollangzzang@gmail.com>
2019-06-22 22:45:49 +0900

    2019-06-22 update-2

--- BFS/BFS/7576.cpp
+++ BFS/BFS/7576.cpp
@@ -1,80 +1,147 @@
 #include <iostream>
 #include <cstring>
 #include <queue>
+#include <vector>
+
+#define pos pair<int,int>
+
+struct toma {
+	int x, y, cnt;
+};
 
 using namespace std;
 
-int M, N; // 2이상 1000이하
+int N, M;
 int map[1001][1001];
-bool visited[1001][1001];
+int visited[1001][1001];
 
 int dx[] = { 0,0,-1,1 };
 int dy[] = { -1,1,0,0 };
 
-typedef struct {
-	int x, y;
-}pt;
+bool isRipe = false;
+int cnt = 0;
+vector <pos> vec;
 
-bool inBoundary(int x, int y) {
-	if (x<0 || y<0 || x>M - 1 || y>N - 1) return false;
+bool isBoundary(int x, int y) {
+
+	if (x < 0 || y <0 || x>N || y>M) return false;
 	return true;
 }
 
-queue<pt> q;
 
-int BFS() {
+int BFS(int x, int y) {
+	queue<toma> tq;
+	tq.push({ 0,0,0 });
 
-	int cnt = 0;
+	while (!tq.empty()) {
+		int cx = tq.front().x;
+		int cy = tq.front().y;
+		int ccnt = tq.front().cnt;
+
+		for (int i = 0; i < 4; i++) {
+			int nx = cx + dx[i];
+			int ny = cy + dy[i];
+			if (map[nx][ny] == 0) {
+				tq.push({ nx,ny,ccnt + 1 });
+			}
+
+		}
+
+	}
+
+
+
+
+
+
+
+	queue <pos> q;
+	
+	if (isRipe == false)
+		return 0;
+
+	q.push({ x,y });
+	visited[x][y] = 1;
 
 	while (!q.empty()) {
 
-		int curX = q.front().x;
-		int curY = q.front().y;
+		int curX = q.front().first;
+		int curY = q.front().second;
 		q.pop();
 
-		cout << curX << curY << endl;
+		for (int i = 0; i < 4; i++) {
+			int nextX = curX + dx[i];
+			int nextY = curY + dy[i];
 
-		for (int j = 0; j < 4; j++) {
-			int nextX = curX + dx[j];
-			int nextY = curY + dy[j];
-
-			cout << "Boundary Value : " << inBoundary(nextX, nextY) << endl;
-			cout << "next map (x,y) : " << map[nextX][nextY] << endl;
-			cout << "M : " << M << " N : " << N << endl;
-			cout << "nextX : " << nextX << " nextY : " << nextY << endl;
-			cout << "!visit : " << !visited[nextX][nextY] << endl;
-
-			if (inBoundary(nextX, nextY) && !map[nextX][nextY] && !visited[nextX][nextY]) {
+			if (isBoundary(nextX, nextY) && visited[nextX][nextY] == 0 && map[nextX][nextY] == 0)
+			{
 				q.push({ nextX, nextY });
-				cout << "execute queue push" << endl;
-				visited[nextX][nextY] = true;
+				visited[nextX][nextY] = 1;
+				
 			}
-			else if (map[nextX][nextY] == -1)
-				continue;
 
 		}
-		cnt++;
-	}
 
-	return cnt;
+	}
+}
+
+
+bool isFullRipe() {
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+
+			if (visited[i][j] == 0)
+				return false;
+
+		}
+	}
+	return true;
 }
 
 int main() {
-
 	cin >> M >> N;
+
+	if (M < 2 || N < 2 || M >1000 || N>1000)
+		return -1;
+
+	memset(map, 0, sizeof(map));
+	memset(visited, 0, sizeof(visited));
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 
 			cin >> map[i][j];
-
+			if (isRipe == false && map[i][j] == 0)
+				isRipe = true;
 			if (map[i][j] == 1)
-				q.push({ i,j });
+				vec.push_back({ i,j });
+			else if (map[i][j] == -1)
+				visited[i][j] = 1;
+
 				
 		}
+
 	}
 
-	cout << BFS();
+	for (int i = 0; i < vec.size(); i++) {
+		BFS(vec[i].first, vec[i].second);
+	}
+
+	if (!isFullRipe()) {
+		cout << "-1";
+		return 0;
+
+	}
+		
+	else if (!isRipe) {
+		cout << "0";
+		return 0;
+
+	}
+		
+	else
+		cout << "cnt : " <<cnt;
 
 	return 0;
 }
