ecd303ac9d54e14e766cb8273c0c5b9b1ec4f099
김기태 <flash1117@naver.com>
2019-07-01 23:48:27 +0900

    2019-07-01 update-2

--- BFS/BFS/14502.cpp
+++ BFS/BFS/14502.cpp
@@ -7,11 +7,13 @@ using namespace std;
 
 int N, M;
 int map[8][8];
+int copymap[8][8];
 bool visited[8][8];
 
 int dx[] = { 0,0,-1,1 };
 int dy[] = { -1,1,0,0 };
 
+vector <pair<int, int>> vec;
 
 void print() {
 
@@ -25,29 +27,91 @@ void print() {
 	}
 }
 
+void copyMap() {
+
+	for (int i = 0; i < N; i++) {
+
+		for (int j = 0; j < N; j++) {
+
+			copymap[i][j] = map[i][j];
+
+		}
+	}
+}
+
+void makeWall(int cnt) {
+	int x1, x2, y1, y2;
+	if (cnt == 3) {
+		copymap[x1][y1] = 0;
+		copymap[x2][y2] = 0;
+	}
+	else {
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+
+				if (map[i][j] == 0) {
+					copymap[i][j] = 1;
+
+					if (cnt == 1) {
+						x1 = i;
+						y1 = j;
+					}
+					else if (cnt == 2) {
+						x2 = i;
+						y2 = j;
+					}
+
+					makeWall(cnt + 1);
+				}
+
+			}
+
+		}
+	}
+}
+
+void BFS() {
+
+	queue <pair<int, int>> q;
+
+
+}
+
 int main() {
 	int wall = 3;
 
 	memset(map, 0, sizeof(map));
+	memset(copymap, 0, sizeof(copymap));
 	memset(visited, false, sizeof(visited));
 
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 
-		for(int j=0; j<M; j++)
+		for (int j = 0; j < M; j++) {
 			cin >> map[i][j];
+			if (map[i][j] == 2)
+				vec.push_back({ i,j });
+		}
+			
 	}
 
-	for (int f = 0; f < N; f++) {
+	copyMap();
 
-		for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) {
 
-			for (int j = 0; j < N; j++) {
+		for (int j = 0; j < N; j++) {
 
-
+			if (map[i][j] == 0) {
+				copymap[i][j] = 1;
+				makeWall(1);
+				BFS();
+				copymap[i][j] = 0;
 			}
 		}
 	}
+
+
 
 	return 0;
 }
