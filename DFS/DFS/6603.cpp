07f21e731ce98b8980e91bd2892dcb87ab202c28
김기태 <flash1117@naver.com>
2019-07-02 18:01:31 +0900

    190702 update-3

---
+++ DFS/DFS/6603.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int k = 0;
+int s[50];
+
+vector <int> vec[100];
+
+void DFS(int index) {
+
+	if (index == 0) return;
+	else {
+
+		for (int i = 0; i < k; i++) {
+
+
+
+		}
+
+	}
+
+}
+
+int main() {
+
+	memset(s, 0, sizeof(s));
+	int temp = 0;
+	while (1) {
+
+		cin >> k;
+		if (k == 0) break;
+
+		for (int i = 0; i < k; i++) {
+			cin >> s[i];
+		}
+	
+	}
+
+	return 0;
+}
