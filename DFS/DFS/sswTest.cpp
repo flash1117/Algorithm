5bff998cc7864805aef69d6ec45408c1ff9f4340
김기태 <flash1117@naver.com>
2019-07-05 04:36:07 +0900

    190705 update-3

---
+++ DFS/DFS/sswTest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int f(int n) {
+	if (n <= 0) return 0;
+	if (n <= 3) return n;
+
+	return f(n - 2) + f(n - 3);
+
+}
+
+int main() {
+
+	int ans = f(8);
+	cout << ans;
+	return 0;
+
+}
