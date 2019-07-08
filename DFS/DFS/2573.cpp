4a682f088d55d9983f4f67b126e83042d3cd306a
김기태 <flash1117@naver.com>
2019-07-09 06:25:29 +0900

    190709 update

--- DFS/DFS/2573.cpp
+++ DFS/DFS/2573.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int N, M;
 int map[301][301];
-int nextMap[301][301];
 bool visited[301][301];
 
 int dx[] = { 0,0,-1,1 };
@@ -27,8 +26,8 @@ void DFS(int x, int y) {
 		int nearX = x + dx[i];
 		int nearY = y + dy[i];
 
-		if (map[nearX][nearY] == 0 && map[x][y] > 0 && nextMap[x][y] > 0)
-			nextMap[x][y] -= 1;
+		if (map[nearX][nearY] == 0 && map[x][y] > 0 && !visited[nearX][nearY])
+			map[x][y] -= 1;
 	}
 
 	for (int i = 0; i < 4; i++) {
@@ -46,7 +45,7 @@ void print() {
 
 		for (int j = 0; j < M; j++) {
 
-			cout << nextMap[i][j] << " ";
+			cout << map[i][j] << " ";
 		}
 		cout << endl;
 	}
@@ -54,8 +53,8 @@ void print() {
 
 int main() {
 
+	int cnt = 0;
 	memset(map, 0, sizeof(map));
-	memset(nextMap, 0, sizeof(nextMap));
 
 	cin >> N >> M;
 
@@ -64,7 +63,6 @@ int main() {
 		for (int j = 0; j < M; j++) {
 
 			cin >> map[i][j];
-			nextMap[i][j] = map[i][j];
 		}
 	}
 
@@ -72,6 +70,7 @@ int main() {
 		area = 0;
 		memset(visited, false, sizeof(visited));
 
+		cnt++;
 		for (int i = 0; i < N; i++) {
 
 			for (int j = 0; j < M; j++) {
@@ -80,21 +79,15 @@ int main() {
 					{
 						DFS(i, j);
 						area++;
+						if (area > 1) {
+							cout << cnt-1;
+							return 0;
+						}
 					}
 			}
 		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				map[i][j] = nextMap[i][j];
-			}
-		}
-
-		if(area >1)
-			break;
 	}
-	cout << area-1;
-	//print();
 
+	
 	return 0;
 }
