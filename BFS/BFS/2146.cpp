a8c54c97f211e5870e379c8ab90cd1f549c36ac6
김기태 <flash1117@naver.com>
2019-07-11 00:45:52 +0900

    190710 update-2

--- BFS/BFS/2146.cpp
+++ BFS/BFS/2146.cpp
@@ -1,80 +1,91 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
 #include <queue>
 
 using namespace std;
 
-int N, area =1;
+int N;
 int map[101][101];
 bool visited[101][101];
 
 int dx[] = { 0,0,-1,1 };
 int dy[] = { -1,1,0,0 };
 
-vector <int> ret;
-
-typedef struct {
-
-	int x, y, cnt;
-}pos;
-
 bool isBoundary(int x, int y) {
-
 	if (x<0 || y<0 || x>N - 1 || y>N - 1) return false;
 	return true;
+
 }
 
-void BFS(int x, int y) {
+void landCheck(int area, int x, int y) {
 
-	queue <pos> q;
-	q.push({ x,y,0 });
+	queue <pair<int, int>> q;
+	q.push({ x,y });
+	visited[x][y] = true;
 
 	while (!q.empty()) {
 
-		int curX = q.front().x;
-		int curY = q.front().y;
-		int ccnt = q.front().cnt;
+		int curX = q.front().first;
+		int curY = q.front().second;
 		q.pop();
 
+		map[curX][curY] = area;
+
 		for (int i = 0; i < 4; i++) {
+
 			int nextX = curX + dx[i];
 			int nextY = curY + dy[i];
 
-			if (map[nextX][nextY] == 0 && isBoundary(nextX, nextY))
-			{
-				q.push({ nextX, nextY, ccnt+1 });
-				
+			if (isBoundary(nextX, nextY) && !visited[nextX][nextY] && map[nextX][nextY] == 1) {
+				q.push({ nextX, nextY });
+				visited[nextX][nextY] = true;
+
 			}
 		}
+
 	}
 }
 
+void print() {
+
+	cout << endl;
+	for (int i = 0; i < N; i++) {
+
+		for (int j = 0; j < N; j++) {
+
+			cout << map[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+
+}
 int main() {
 
-	memset(map, 0, sizeof(map));
+	int area = 1;
+
 	memset(visited, false, sizeof(visited));
 	cin >> N;
 
 	for (int i = 0; i < N; i++) {
 
 		for (int j = 0; j < N; j++) {
-
 			cin >> map[i][j];
-			if (map[i][j] == 1)
-				visited[i][j] = true;
+
 		}
 	}
+
 
 	for (int i = 0; i < N; i++) {
-
 		for (int j = 0; j < N; j++) {
-			
-			if (map[i][j] == 1 && !visited[i][j]) {
-				BFS(i, j);
-			}			
+			if (!visited[i][j] && map[i][j] == 1)
+			{
+				landCheck(area, i, j);
+				area++;
+			}
 		}
 	}
 
+	print();
 	return 0;
 }
