e1eec4bb540fc1e6d010fa30809c279a94796002
김기태 <flash1117@naver.com>
2019-07-11 01:48:35 +0900

    190711 update

--- BFS/BFS/2146.cpp
+++ BFS/BFS/2146.cpp
@@ -4,12 +4,17 @@
 
 using namespace std;
 
-int N;
+int N, minLength = 101;
 int map[101][101];
 bool visited[101][101];
 
 int dx[] = { 0,0,-1,1 };
 int dy[] = { -1,1,0,0 };
+
+typedef struct {
+	int x, y, cnt;
+
+}pos;
 
 bool isBoundary(int x, int y) {
 	if (x<0 || y<0 || x>N - 1 || y>N - 1) return false;
@@ -60,6 +65,38 @@ void print() {
 
 
 }
+
+void BFS(int l_x, int l_y, int area) {
+
+	queue <pos> q;
+	q.push({ l_x,l_y,0 });
+	visited[l_x][l_y] = true;
+
+	while (!q.empty()) {
+		int curX = q.front().x;
+		int curY = q.front().y;
+		int ccnt = q.front().cnt;
+
+		q.pop();
+
+		if (map[curX][curY] != area && map[curX][curY] > 0) {
+			if (minLength > ccnt) minLength = ccnt;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nextX = curX + dx[i];
+			int nextY = curY + dy[i];
+
+			if (isBoundary(nextX, nextY) && map[nextX][nextY] != area && !visited[nextX][nextY])
+			{
+				q.push({ nextX, nextY, ccnt + 1 });
+				visited[nextX][nextY] = true;
+			}
+		}
+
+	}
+
+}
 int main() {
 
 	int area = 1;
@@ -86,6 +123,23 @@ int main() {
 		}
 	}
 
-	print();
+	
+	
+	while (area--) {
+		memset(visited, false, sizeof(visited));
+
+		for (int i = 0; i < N; i++) {
+
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] == area)
+				{
+					BFS(i,j,area);
+				}
+			}
+		}
+	}
+
+	cout << minLength;
+	//print();
 	return 0;
 }
