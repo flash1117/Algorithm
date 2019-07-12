c2ef19a3dac4e00c70af91b47eb6bd01d419fc29
김기태 <flash1117@naver.com>
2019-07-12 11:12:09 +0900

    190712 update

---
+++ DFS/DFS/2668.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int N;
+vector <int> vec;
+bool visited[101];
+int start;
+
+vector <int> process;
+vector <int> ret;
+void DFS(int x)
+{
+	if (visited[x] && x != start) {
+
+		for (int i = 1; i < process.size(); i++) {
+			if(process[i] != start)
+				visited[process[i]] = false;
+		}
+		return;
+	}
+	if (x == vec[x - 1] && !visited[x]) {
+		ret.push_back(x);
+		return;
+	}
+		
+	if (!visited[x])
+		process.push_back(x);
+	
+	visited[x] = true;
+
+	if (vec[x - 1] == start) {
+		for (int i = 0; i < process.size(); i++) {
+			ret.push_back(process[i]);
+		}
+			
+		
+		return;
+	}
+
+	DFS(vec[x - 1]);
+
+}
+
+void Init() {
+
+	for (int i = 0; i < process.size(); i++)
+		process.pop_back();
+
+}
+
+int main() {
+
+	memset(visited, false, sizeof(visited));
+	int input;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		vec.push_back(input);
+
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			start = i;
+			DFS(i);
+			Init();
+		}
+			
+
+	}
+	
+	cout << endl << ret.size() << endl;
+
+	for (int i = 0; i < ret.size(); i++)
+		cout << ret[i] << endl;
+	return 0;
+}
