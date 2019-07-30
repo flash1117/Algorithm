65b31dc4b5b284456cf16ef896e7a45f5f426c1e
김기태 <flash1117@naver.com>
2019-07-30 17:52:39 +0900

    190730 update-2

--- BFS/BFS/12761.cpp
+++ BFS/BFS/12761.cpp
@@ -15,13 +15,101 @@ bool isBoundary(int x)
 
 int BFS() {
 
+	queue <pair<int,int>> q;
+	visited[N] = true;
+	q.push(make_pair(N, 0));
 
+	while (!q.empty()) {
 
+		int cur = q.front().first;
+		int cnt = q.front().second;
+		q.pop();
+
+		if (cur == M) return cnt;
+
+		for (int i = 0; i < 8; i++) {
+			int next;
+			switch (i)
+			{
+			case 1: {
+				next = cur - 1;
+				if (isBoundary(next) && !visited[next]) {
+					q.push(make_pair(next, cnt + 1));
+					visited[next] = true;
+				}
+				break;
+			}
+			case 2:
+			{
+				next = cur + 1;
+				if (isBoundary(next) && !visited[next]) {
+					q.push(make_pair(next, cnt + 1));
+					visited[next] = true;
+				}
+				break;
+			}
+			case 3:
+			{
+				next = cur - A;
+				if (isBoundary(next) && !visited[next]) {
+					q.push(make_pair(next, cnt + 1));
+					visited[next] = true;
+				}
+				break;
+
+			}
+			case 4: {
+				next = cur + A;
+				if (isBoundary(next) && !visited[next]) {
+					q.push(make_pair(next, cnt + 1));
+					visited[next] = true;
+				}
+				break;
+			}
+			case 5:
+			{
+				next = cur + B;
+				if (isBoundary(next) && !visited[next]) {
+					q.push(make_pair(next, cnt + 1));
+					visited[next] = true;
+				}
+				break;
+			}
+			case 6: {
+				next = cur - B;
+				if (isBoundary(next) && !visited[next]) {
+					q.push(make_pair(next, cnt + 1));
+					visited[next] = true;
+				}
+				break;
+			}
+			case 7: {
+				next = cur * A;
+				if (isBoundary(next) && !visited[next]) {
+					q.push(make_pair(next, cnt + 1));
+					visited[next] = true;
+				}
+				break;
+			}
+			case 8: {
+				next = cur * B;
+				if (isBoundary(next) && !visited[next]) {
+					q.push(make_pair(next, cnt + 1));
+					visited[next] = true;
+				}
+				break;
+			}
+			default:
+				break;
+			}
+
+		}
+	}
 }
 
 int main() {
 
 	cin >> A >> B >> N >> M;
 
-
+	cout << BFS();
 }
