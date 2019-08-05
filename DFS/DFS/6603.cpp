e1f8499bf4a5f18e25a66f2653db4bc523900067
김기태 <flash1117@naver.com>
2019-08-05 17:06:41 +0900

    190805 6603 update

--- DFS/DFS/6603.cpp
+++ DFS/DFS/6603.cpp
@@ -8,40 +8,32 @@ bool visited[13];
 
 void DFS(int depth, int cnt) {
 
-	if (depth == 6) {
+	if (cnt == 6) {
 
 		for (int i = 0; i < 6; i++)
-				cout << ret[i] << " ";
+			cout << ret[i] << " ";
 		cout << endl;
 		return;
 	}
-	
-	for (int i = cnt; i < k; i++) {
 
-		ret[depth] = s[i];
-		DFS(depth + 1, cnt + 1);
-	//	ret[depth] = -1;
+
+	for (int i = depth; i < k; i++) {
+		ret[cnt] = s[i];
+		DFS(i+1, cnt + 1);
 	}
+
 }
 
 
 int main() {
 
-	while (1) {
+	while (cin >> k && k) {
 
-		cin >> k;
-		if (k == 0) break;
-		else {
-
-			for (int i = 0; i < k; i++) 
-				cin >> s[i];
-			
-		}
-
+		for (int i = 0; i < k; i++) 
+			cin >> s[i];
 
 		DFS(0,0);
 		cout << endl;
-		memset(visited, false, sizeof(visited));
 		memset(s, 0, sizeof(s));
 
 	}
