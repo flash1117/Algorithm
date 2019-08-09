b650b0cbcf26bc8456c5d2e2fd5369d553acb65d
김기태 <flash1117@naver.com>
2019-08-09 17:29:56 +0900

    190809 update-2

--- Dynamic Programming/Dynamic Programming/1463.cpp
+++ Dynamic Programming/Dynamic Programming/1463.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
-#include <cstring>
-#include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int N;
+int ary[1000001];
 
-int DP() {
+void DP() {
 
+	ary[1] = 0;
+	for (int i = 2; i <= N; i++) {
+		ary[i] = ary[i - 1] + 1;
+		if (i % 3 == 0) {
+			ary[i] = min(ary[i],ary[i / 3] + 1);
+		}
+		else if (i % 2 == 0) {
+			ary[i] = min(ary[i],ary[i / 2] + 1);
+		}
 
-
+	}
 }
 
 int main() {
@@ -17,5 +25,7 @@ int main() {
 	cin >> N;
 	DP();
 
+
+	cout << ary[N];
 	return 0;
 }
