a70b82fbad2023c4b1ce5486bb9e887860affc53
김기태 <flash1117@naver.com>
2019-08-13 16:07:23 +0900

    190813 update

--- Simulation/Simulation/1966.cpp
+++ Simulation/Simulation/1966.cpp
@@ -1,46 +1,53 @@
 #include <iostream>
-#include <queue>
+#include <deque>
 
 using namespace std;
 
 int N, M;
+deque <pair<int, int>> dq;
 
-int main() {
+bool isMax(int a) {
 
-	int T, input;
-	pair<int,int> dst;
+	for (int i = 0; i < dq.size(); i++) 
+		if (dq[i].first > a) return false;
+	return true;
+}
+
+int main()
+{
+	
+	int T, input, cnt=0;
 	cin >> T;
 	while (T--) {
-		int cnt = 0;
-		cin >> N >> M;
 
-		queue<pair<int, int>> q;
+		cin >> N >> M;
 		for (int i = 0; i < N; i++) {
 			cin >> input;
-			if (i == M) {
-				dst.first = input;
-				dst.second = i;
-			}
-			q.push(make_pair(input, i));
+			dq.push_back(make_pair(input, i));
 		}
 
-		while (!q.empty()) {
+		while(1) {
+			if (isMax(dq[0].first)) {
+				cnt++;
+				if (dq.front().second == M) {
+					cout << cnt << endl;
+					break;
+				}
+				dq.pop_front();
 
-			int cur = q.front().first;
-			int index = q.front().second;
-			q.pop();
-		
-			if (cur == dst.first && index == dst.second)
-			{
-				cout << cnt << endl;
-				break;
+			}
+			else {
+				pair<int, int> p;
+				p = dq.front();
+				dq.pop_front();
+				dq.push_back(p);
+
 			}
 
-		
 		}
 
-
-
+		cnt = 0;
+		dq.clear();
 	}
 
 	return 0;
