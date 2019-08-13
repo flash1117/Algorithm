6fb63e0616679b677380f5e9d5e104ab84a1a8ec
김기태 <flash1117@naver.com>
2019-08-13 18:16:01 +0900

    190814 update

---
+++ SW_Expert/SW_Expert/12100.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+#define MAXSIZE 21
+
+int map[MAXSIZE][MAXSIZE];
+int N;
+
+void solve() {
+
+	
+
+}
+
+
+int main() {
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+
+		}
+	}
+
+	return 0;
+}
