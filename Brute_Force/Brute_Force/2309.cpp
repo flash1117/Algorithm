63ad901a87ddcff1468291a8acd2829e9783c626
김기태 <flash1117@naver.com>
2019-08-15 22:58:40 +0900

    190815 update

---
+++ Brute_Force/Brute_Force/2309.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define Endl "\n"
+
+vector <int> hobit;
+int Ary[7];
+void solve(int depth , int cnt) {
+
+	if (depth == 7) {
+		int sum = 0;
+		for (int i = 0; i < 7; i++)
+			sum += Ary[i];
+		
+		if (sum == 100)
+			for (int i = 0; i < 7; i++)
+				cout << Ary[i] << Endl;
+		return;
+	}
+
+	for (int i = depth; i < hobit.size(); i++) {
+
+
+		Ary[depth] = hobit[i];
+		if(Ary[depth] > Ary[depth-1] || depth == 0)
+			solve(depth + 1, cnt + 1);
+	}
+}
+
+int main() {
+
+	int input;
+	for (int i = 0; i < 9; i++) {
+
+		cin >> input;
+		hobit.push_back(input);
+		
+	}
+
+	sort(hobit.begin(), hobit.end());
+	solve(0,0);
+
+	return 0;
+}
