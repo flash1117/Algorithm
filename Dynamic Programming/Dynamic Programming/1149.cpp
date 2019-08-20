d086174090df61d46a5a5ece34fac62b9988b00d
김기태 <flash1117@naver.com>
2019-08-21 00:31:42 +0900

    190821 update

--- Dynamic Programming/Dynamic Programming/1149.cpp
+++ Dynamic Programming/Dynamic Programming/1149.cpp
@@ -1,54 +1,56 @@
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
 int N;
-int map[1001][3];
+int dp[1001];
+int home[1001][3];
+int sum;
 
-int DP(int color, int cnt ,int sum) {
+void solve() {
 
-	if (cnt == N) return sum;
-
-	int min = 1001;
-	if (sum == 0 && cnt ==0) {
-
-		sum = map[cnt++][color];
-		DP(color, cnt, sum);
-	}
-	else {
-
-		for (int i = 0; i < 3; i++) {
-
-			if (i == color) continue;
-			else {
-				if (min > map[cnt][i]) {
-					min = map[cnt][i];
-					color = i;
-				}
-			}
-		}
-		cnt++;
-		sum += min;
-		DP(color, cnt, sum);
-	}
+	int prev = 0;
 	
+
+	for (int i = 0; i < N; i++) {
+
+		int cost = 1001;
+
+		for (int j = 0; j < 3; j++) {
+
+			if (j != prev || i == 0) {
+
+				if (home[i][j] < cost) {
+					cost = home[i][j]; prev = j;
+				}
+				
+			}
+
+
+		}
+		sum += cost;
+	}
+
+
 }
 
+
 int main() {
-
-	int ret = 1000001;
-	int temp;
+	int input;
 	cin >> N;
-	for (int i = 0; i < N; i++) 
-		cin >> map[i][0] >> map[i][1] >> map[i][2];
+	
+	for (int i = 0; i < N; i++) {
 
-	for (int i = 0; i < 3; i++) {
-		temp = DP(i, 0, 0);
-		if (ret > temp) ret = temp;
+		for (int j = 0; j < 3; j++) {
+			cin >> input;
+			home[i][j] = input;
+		}
 	}
 	
-	cout << ret;
+	solve();
 
+
+	cout << sum;
 	return 0;
+
 }
