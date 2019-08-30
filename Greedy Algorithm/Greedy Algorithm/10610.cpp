d699524b4cb6b25fffc1dc93378343cba8873960
김기태 <flash1117@naver.com>
2019-08-30 18:17:18 +0900

    190830 update-4

---
+++ Greedy Algorithm/Greedy Algorithm/10610.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N;
+
+int solve() {
+
+	while (1) {
+		if (N < 10) break;
+		else {
+
+		}
+
+	}
+
+
+
+}
+
+int main() {
+
+	cin >> N;
+
+
+	return 0;
+}
