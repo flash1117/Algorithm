0e8c0f0e95f9269b8366551cc0d476556e786bdd
김기태 <flash1117@naver.com>
2019-08-31 18:20:13 +0900

    190831 update-2

--- Greedy Algorithm/Greedy Algorithm/10610.cpp
+++ Greedy Algorithm/Greedy Algorithm/10610.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
-string N;
 
-int solve() {
+bool cmp(char a, char b) {
 
-	int ret = 0, pos =1;
-	vector <int> vec;
-
-	for (int i = 0; i < N.length(); i++) 
-		vec.push_back(N[i] - '0');
-
-	sort(vec.begin(), vec.end());
-
-	for (int i = 0; i < vec.size(); i++)
-	{
-		ret += vec[i] * pos;
-		pos *= 10;
-	}
-	if (ret % 30 == 0) return ret;
-	
-	return -1;
-
+	if (a > b) return true;
+	return false;
 }
 
-int main() {
 
+int main() {
+	
+	string N;
 	cin >> N;
-	cout << solve();
+
+	long long ret = 0;
+	bool isZero = false;
+
+	for (int i = 0; i < N.length(); i++) {
+
+		ret += (N[i] - '0');
+		if (!(N[i] - '0')) isZero = true;
+	}
+
+	if (ret % 3 || !isZero) cout << -1;
+
+	else {
+		sort(N.begin(), N.end(), cmp);
+		cout << N;
+	}
 
 	return 0;
 }
