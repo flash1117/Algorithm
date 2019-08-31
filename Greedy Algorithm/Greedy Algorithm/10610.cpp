bde21a8f7c013f4351765b5f0967ea18f0fffdfc
김기태 <flash1117@naver.com>
2019-08-31 17:21:09 +0900

    190831 update

--- Greedy Algorithm/Greedy Algorithm/10610.cpp
+++ Greedy Algorithm/Greedy Algorithm/10610.cpp
@@ -4,26 +4,33 @@
 
 using namespace std;
 
-int N;
+string N;
 
 int solve() {
 
-	while (1) {
-		if (N < 10) break;
-		else {
+	int ret = 0, pos =1;
+	vector <int> vec;
 
-		}
+	for (int i = 0; i < N.length(); i++) 
+		vec.push_back(N[i] - '0');
 
+	sort(vec.begin(), vec.end());
+
+	for (int i = 0; i < vec.size(); i++)
+	{
+		ret += vec[i] * pos;
+		pos *= 10;
 	}
-
-
+	if (ret % 30 == 0) return ret;
+	
+	return -1;
 
 }
 
 int main() {
 
 	cin >> N;
-
+	cout << solve();
 
 	return 0;
 }
