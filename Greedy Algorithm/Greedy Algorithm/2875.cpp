07d63971ee6c17a9b1af76c348904146777103ab
김기태 <flash1117@naver.com>
2019-09-02 23:55:48 +0900

    190902 update

---
+++ Greedy Algorithm/Greedy Algorithm/2875.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M, K;
+int maxTeam;
+
+void solve() {
+
+	if (N > 2 * M) {
+
+		maxTeam = M;
+
+	}
+
+
+
+}
+
+
+int main() {
+
+	cin >> N >> M >> K;
+	solve();
+
+
+
+	return 0;
+}
