7ffecc5951cb255823faa9f9f77ee0c847b16afe
김기태 <flash1117@naver.com>
2019-09-10 23:53:35 +0900

    190910 update

--- Brute_Force/Brute_Force/2231.cpp
+++ Brute_Force/Brute_Force/2231.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int N;
-vector <int > digit;
 
-void solve(){
-	int q = N;
-	for (int i = 0; i < 6; i++) {
+int searchCreate() {
 
-		digit.push_back(q % 10);
-		if (q < 10) break;
-		q = q / 10;
-		
+	int creator = 1;
+
+	while (1) {
+
+		int temp = creator;
+		int sum = creator;
+
+		while (temp) {
+
+			sum += temp % 10;
+			temp /= 10;
+		}
+
+		if (creator == N || sum == N)
+			break;
+
+		creator++;
+
 	}
+	return creator;
 
 }
 
 int main() {
 
 	cin >> N;
+	int result = searchCreate();
 
-	solve();
+	if (result == N)
+		cout << 0;
+	else
+		cout << result;
 	return 0;
+
 }
