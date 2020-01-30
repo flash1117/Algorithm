de4cac7bf51d6ffbf53337f66273a7aebdfcbb44
김기태 <flash1117@naver.com>
2020-01-30 21:56:11 +0900

    update boj

--- Brute_Force/Brute_Force/1107.cpp
+++ Brute_Force/Brute_Force/1107.cpp
@@ -11,27 +11,50 @@ int pick[7];
 
 void solve(int nLength , int depth , int cnt) {
 
-	if (depth > num.size()) return;
-	else if (cnt == nLength) {
-		int sum = 0;
-		int digit = 1;
-		for (int i = cnt - 1; i >= 0; i--) {
-			sum += digit * num[pick[i]];
-			digit *= 10;
+	if (depth > nLength) return;
+	else if (cnt == nLength ) {
+		if (num[pick[0]] != 0) {
+			int sum = 0;
+			int digit = 1;
+			for (int i = cnt - 1; i >= 0; i--) {
+				sum += digit * num[pick[i]];
+				digit *= 10;
+			}
+			//cout << sum << "\n";
+			if (sum > N) ret = ret > (sum - N + nLength) ? (sum - N + nLength) : ret;
+			else if (sum == N) ret = nLength;
+			else  ret = ret > (N - sum + nLength) ? (N - sum + nLength) : ret;
 		}
-		
-		if (sum > N) ret = ret > (sum - N + nLength) ? (sum - N + nLength) : ret;
-		else if (sum == N) ret = nLength;
-		else  ret = ret > (N -sum + nLength) ? (N - sum + nLength) : ret;
+		else {
+			int zeroCnt = 0;
+			int sum = 0;
+			int digit = 1;
+			for (int i = 0; i < cnt; i++) {
+				if (num[pick[i]] == 0) zeroCnt++;
+				else break;
+			}
+
+			for (int i = cnt - 1; i >= 0; i--) {
+				sum += digit * num[pick[i]];
+				digit *= 10;
+			}
+			//cout << sum << "\n";
+			if (sum > N) ret = ret > (sum - N + nLength - zeroCnt) ? (sum - N + nLength - zeroCnt) : ret;
+			else if (sum == N) ret = nLength - zeroCnt;
+			else  ret = ret > (N - sum + nLength - zeroCnt) ? (N - sum + nLength - zeroCnt) : ret;
+
+		}
 
 		return;
 	}
 
-	pick[cnt] = depth;
-	solve(nLength, depth + 1, cnt + 1);
-
-	solve(nLength, depth + 1, cnt + 1);
-
+	for (int i = 0; i < num.size(); i++) {
+	
+		pick[cnt] = i;
+		solve(nLength, depth + 1, cnt + 1);
+		
+	}
+	
 	return;
 }
 
