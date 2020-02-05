dd477ba8813970b9ae6ef6d90e8ee2634ef77d88
김기태 <flash1117@naver.com>
2020-02-05 16:12:21 +0900

    update boj

--- Simulation/Simulation/15685.cpp
+++ Simulation/Simulation/15685.cpp
@@ -1,13 +1,59 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int x, y;
-void solve();
+typedef struct {
+	int x,y,d,g;
+}dragonCurve;
 
-int main() {
+int N;
+int map[101][101];
+int dx[] = { 0,-1,0,1 };
+int dy[] = { 1,0,-1,0 };
 
+
+// 3 0 1 0 1 2 1 0   1 2 3 2 1 2 1 0
+
+
+void solve(int x, int y, int d, int g) {
 	
+	vector <int> dir;
+	int nextX = x + dx[d];
+	int nextY = y + dy[d];
 
+	map[x][y] = 1;
+	map[nextX][nextY] = 1;
+	dir.push_back(d);
+	while (g--) {
+		int bound = dir.size();
+		for (int i = bound-1; i >=0; i--) {
+			int tmp = dir[i] + 1;
+			tmp = tmp > 3 ? 0 : tmp;
+			dir.push_back(tmp);
+			nextX += dx[tmp];
+			nextY += dy[tmp];
+			map[nextX][nextY] = 1;
+		}
+	}
+
+}
+int main() {
+	int x, y, g, d;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y >> d >> g;
+		solve(y, x, d, g);
+	}
+	int cnt = 0;
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 100; j++) {
+			if (map[i][j] == 1 && map[i + 1][j] == 1 && map[i][j + 1] && map[i + 1][j + 1]) cnt++;
+		}
+	}
+
+	if (map[99][99] == 1 && map[100][99] == 1 && map[99][100] && map[100][100]) cnt++;
+	cout << cnt << "\n";
 	return 0;
 }
---
+++ Simulation/Simulation/17779.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef struct {
+	int x, y, d1, d2;
+}Bound;
+
+int N;
+int map[20][20];
+vector <Bound> Area;
+
+bool isBoundary(int x, int y) {
+	if (x<0 || y<0 || x>N - 1 || y>N - 1) return false;
+	return true;
+}
+
+bool setBound(Bound input) {
+
+	if (!isBoundary(input.x + input.d1, input.y - input.d1)) return false;
+	else if (!isBoundary(input.x + input.d2, input.y + input.d2)) return false;
+
+	return true;
+}
+
+int main() {
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+
+			for (int d1 = 1; d1 < N; d1++) {
+				for (int d2 = 1; d2 < N; d2++) {
+					Bound input = { i,j,d1,d2 };
+					bool isBound = setBound(input);
+					if (isBound) Area.push_back(input);
+				}
+			}
+
+		}
+	}
+
+
+	return 0;
+}
