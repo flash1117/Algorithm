3bb902ad861bbb2bcbb122c73436202401bd48a5
김기태 <flash1117@naver.com>
2020-04-27 16:13:19 +0900

    update algorithm

--- BOJ/BOJ/15651.cpp
+++ BOJ/BOJ/15651.cpp
@@ -4,41 +4,37 @@
 using namespace std;
 
 int N, M;
+int num[] = { 1,2,3,4,5,6,7 };
 int pick[7];
-vector <int> vec;
 
 void solve(int depth, int cnt) {
 
 	if (depth > N) return;
 	else if (cnt == M) {
 
-		for (int i = 0; i < M; i++) {
+		for (int i = 0; i < M; i++)
 			cout << pick[i] << " ";
-		}
 		cout << "\n";
+
 		return;
 	}
 
-
-	for (int i = depth; i < N; i++) {
-
-		if (depth < vec.size()) {
-			pick[cnt] = vec[depth];
-			solve(depth + 1, cnt + 1);
-			solve(depth + 1, cnt);
-		}
+	for (int i = 0; i < N; i++) {
+		
+		pick[cnt] = i + 1;
+		solve(depth + 1, cnt + 1);
+		 
 	}
-	
-	return;
+
 }
 
-int main() {
+int main(void) {
 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	cin >> N >> M;
-	for (int i = 0; i < N; i++)
-		vec.push_back(i + 1);
 
 	solve(0, 0);
-
+	
 	return 0;
 }
