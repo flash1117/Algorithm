243c476f7148a0276f2b7d7caa63e4aa074813b3
김기태 <flash1117@naver.com>
2020-07-16 20:57:24 +0900

    boj 5554 solve

---
+++ Problem/Problem/5554.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int input1, input2, input3, input4;
+
+	cin >> input1 >> input2 >> input3 >> input4;
+
+	cout << (input1 + input2 + input3 + input4) / 60 << endl<<(input1 + input2 + input3 + input4) % 60 << endl;
+
+
+	return 0;
+}
