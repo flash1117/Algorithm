ae970088db4baba0e1c0408f0e5d60e72e44f50e
김기태 <flash1117@naver.com>
2020-08-10 00:06:11 +0900

    boj 15684

--- Simulation/Simulation/15684.cpp
+++ Simulation/Simulation/15684.cpp
@@ -5,8 +5,9 @@ using namespace std;
 const int INF = 987654321;
 
 int N, M, H;
-int map[11][31];
+int map[31][31];
 int minCnt = INF;
+
 void print() {
 	cout << "\n";
 	for (int i = 1; i <= N; i++) {
@@ -18,18 +19,18 @@ void print() {
 }
 
 int ladderClimb(int index) {
-	
+	int cur = index;
 	for (int i = 1; i <= H; i++) {
-		if (map[i][index] == 1) index++;
-		else if (map[i][index] == -1) index--;
+		if (map[i][cur] == 1) cur++;
+		else if (map[i][cur-1] == 1) cur--;
 	}
 
-	return index;
+	return cur;
 }
  
 
 void setLadder(int index, int cnt) {
-	if (cnt > 4) return;
+	if (cnt >= 4) return;
 //	print();
 	bool isAnswer = true;
 	for (int i = 1; i <= N; i++) {
@@ -44,30 +45,27 @@ void setLadder(int index, int cnt) {
 		return;
 	} 
 
-	if (cnt == 4) return;
-
 	for (int j = index; j <= H; j++) {
 		for (int i = 1; i < N; i++) {
 
-			if (map[j][i] == 0 && map[j][i + 1] == 0) {
+			if (map[j][i-1] ==0 && map[j][i] == 0 && map[j][i + 1] == 0) {
 
 				map[j][i] = 1;
-				map[j][i + 1] = -1;
 				setLadder(j, cnt + 1);
 				map[j][i] = 0;
-				map[j][i + 1] = 0;
 			}
 
 		}
 	}
-	
-	
 
 	return;
 }
 
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	cin >> N >> M >> H;
 
@@ -75,19 +73,16 @@ int main() {
 		int x, y;
 		cin >> x >> y;
 		map[x][y] = 1;
-		map[x][y + 1] = -1;
 
 	}
 
 
 	setLadder(1,0);
 
-	if (minCnt == INF || minCnt > 3) {
+	if (minCnt == INF) {
 		cout << -1 << "\n";
 	}
 	else cout << minCnt << "\n";
 		
-	
-
 	return 0;
 }
